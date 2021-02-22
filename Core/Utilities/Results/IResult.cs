734c4a144a174d42b2b5520245f553a688f6d180
Bahadır Versan <versan412010@hotmail.com>
2021-02-22 03:00:06 +0300

    Add project files.

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    // Temel voidler için başlangıç
+    public interface IResult
+    {
+        // get; --> sadece okunabilir
+        bool Success { get; } 
+        string Message { get; }
+    }
+}
